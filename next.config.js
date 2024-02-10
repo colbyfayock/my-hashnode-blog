3ed035bb99222358c46e72384bdb05d318ae752c
Colby Fayock <fayock@gmail.com>
2024-02-09 20:14:32 -0500

    Step 2: Querying Hashnode posts and looping through them on a page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.hashnode.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,72 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import { query } from '@/lib/hashnode';
+
 import Container from '@/components/Container';
 
 export default async function Home() {
+  const { data: { publication } } = await query({
+    query: `
+      query($host: String!) {
+        publication(host: $host) {
+          posts(first: 10) {
+            edges {
+              node {
+                coverImage {
+                  url
+                }
+                id
+                publishedAt
+                slug
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      host: 'spacejelly.hashnode.dev'
+    }
+  });
+
+  interface Post {
+    coverImage: {
+      url: string;
+    };
+    id: string;
+    publishedAt: string;
+    slug: string;
+    title: string;
+  }
+
+  const posts: Array<Post> = publication.posts.edges.map(({ node }: { node: Post }) => node);
+
   return (
     <>
       <Container className="max-w-4xl">
         <ul>
-          <li className="grid sm:grid-cols-2 gap-8 mb-16">
-            <div>
-              <Image width="600" height="400" className="rounded border border-zinc-200" src="/images/placeholder.jpg" alt="" />
-            </div>
-            <div>
-              <h2 className="text-2xl pb-5 border-b-2 mb-5">
-                Title
-              </h2>
-              <p className="text-zinc-500">
-                {
-                  new Date().toLocaleDateString('en-us', {
-                    weekday: 'long',
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric'
-                  })
-                }
-              </p>
-            </div>
-          </li>
+          { posts.map((post) => {
+            return (
+              <li key={post.id} className="grid sm:grid-cols-2 gap-8 mb-16">
+                <Image width="600" height="400" className="rounded border border-zinc-200" src={post.coverImage.url} alt="" />
+                <div>
+                  <h2 className="text-2xl pb-5 border-b-2 mb-5">{ post.title }</h2>
+                  <p className="text-zinc-500">
+                    {
+                      new Date(post.publishedAt).toLocaleDateString('en-us', {
+                        weekday: 'long',
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric'
+                      })
+                    }
+                  </p>
+                </div>
+              </li>
+            )
+          }) }
         </ul>
       </Container>
     </>
